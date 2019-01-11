3e7299ab3acc9273da1bfcf59ec5df6918be0548
jsymmonz <45784003+jsymmonz@users.noreply.github.com>
2019-01-11 09:38:01 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,13 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  host {'web.puppet.fm':
+    ensure => present,
+    ip     => '172.18.0.3',
+   
+   }
+   host {'db.puppet.fm':
+    ensure => present,
+    ip     => '172.18.0.2',
+   }
 }
